558c762974ec6a1903a3b7bb07d35f8a90fe4b9a
Valters-Soms-221RDB325 <124680753+Valters-Soms-221RDB325@users.noreply.github.com>
2023-02-14 16:08:56 +0200

    Tīrāks main.py
    aizvitoju print un exit(0) ar return. Vismaz uz replita nedod vairs atpakaļ neko sarkanu

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
+
 # funkcija atgriež true ja iekavas sakrīt, false ja nesakrīt
 def are_matching(left, right):
   return (left + right) in ["()", "[]", "{}"]
+
 
 # funkcija kas paņem tekstu
 def find_mismatch(text):
@@ -19,21 +21,22 @@ def find_mismatch(text):
     # ja simbols ir ), ], } tad veic 2 pārbaudes [lai izvadītu pirmo neaizvērto iekavu]
     if next in ")]}":
       # 1) Ja steks ir tukšs, vai (pēdējais pievienotais) elements nav vienāds ar (pašreizējo simbolu 'next'):
-      if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-        print(i + 1)
-        exit(0)
+      if not opening_brackets_stack or not are_matching(
+          opening_brackets_stack[-1].char, next):
+        return (i + 1)
       # 2) Ja (pēdējais pievienotais) elemants ir vienāds ar (pašreizējo simbolu 'next'), tas tiek izņemts
       opening_brackets_stack.pop()
+
 
 def main():
   text = input()
   mismatch = find_mismatch(text)
   # Printing answer, write your code here
   if not mismatch:
-    print("Success")
-    exit(0)
+    return print("Success")
   else:
-    print(mismatch)
+    return print(mismatch)
+
 
 if __name__ == "__main__":
   main()
